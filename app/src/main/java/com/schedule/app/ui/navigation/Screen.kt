0abb08a6f82e1f01eaa6ab50158a06db31ec884b
LomKich <marnik240808@gmail.com>
2026-07-22 23:56:36 +0400

    fix

--- app/src/main/java/com/schedule/app/ui/navigation/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.schedule.app.ui.navigation
-
-sealed class Screen(val route: String) {
-    object Files    : Screen("files")
-    object Bells    : Screen("bells")
-    object Schedule : Screen("schedule")
-    object Settings : Screen("settings")
-}
